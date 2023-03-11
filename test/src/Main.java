53201bc89e2b642643ae9bc2df926473732312cf
Subham_Main <smahapatra007@gmail.com>
2023-03-11 12:38:10 +0530

    first commit

---
+++ test/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String var  = "Sidhartha";
+        for(int i = 0; i<var.length();i++){
+            char character = var.charAt(i);
+            System.out.println(character);
+        }
+    }
+}
